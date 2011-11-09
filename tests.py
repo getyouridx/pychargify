d20d20bda59c8f6f52338de546fdb8ae4a448f76
Peter Vessenes <vessenes@gmail.com>
2011-11-09 11:40:59 -0800

    Update tests.py to be more pythonic and nicer looking

--- tests.py
+++ tests.py
@@ -1,79 +1,77 @@
 #!/usr/bin/env python
+''' Test the Pychargify Library.
+    Edit my chargify = line with API key, then run me.
+'''
 
 import api
 
-def print_spacer():
-    print ""
-    print "################################################################################"
-    print ""
+
+def print_spacer(repeat=70):
+    ''' Print a spacer line, 70 chars across '''
+    print "\n" + "#" * repeat + "\n"
+
 
 def print_product(item):
-    print "#########################################"
-    print "# '" + item.name + "' Product"
-    print "#########################################"
-    print "Id:              " + item.id
-    print "Price in Cents:  " + item.price_in_cents
-    print "Name:            " + item.name
-    print "Handle:          " + item.handle
-    print "Accounting Code: " + item.accounting_code
-    print "Interval Unit:   " + item.interval_unit
-    print "Interval:        " + item.interval
-    print " "
-    
+    ''' Print out a little formatted product info item. '''
+    product_info = ''' #########################################
+ ''' + item.name + ''' Product
+#########################################
+Id:              ''' + item.id + '''
+Price in Cents:  ''' + item.price_in_cents + '''
+Name:            ''' + item.name + '''
+Handle:          ''' + item.handle + '''
+Accounting Code: ''' + item.accounting_code + '''
+Interval Unit:   ''' + item.interval_unit + '''
+Interval:        ''' + item.interval + '''
+'''
+    print product_info
 
+
+def run_test():
+    ''' Init the API, and run some sample queries. '''
 # Initialize the API with the API Key and the sub domain
-chargify = api.Chargify('YOUR-API-KEY', 'YOUR-SUB-DOMAIN')
+    chargify = api.Chargify('YOUR-API-KEY', 'YOUR-SUB-DOMAIN')
 
 # Get All Products
-#print "Get All Products: "
-#print ""
-#for item in chargify.Product().getAll():
-#    print_product(item)
+    print "Get All Products: \n"
+    for item in chargify.Product().getAll():
+        print_product(item)
 
+    print_spacer()
+    # Get a Single Product by its ID
+    #print "Get a Product By its ID: "
+    #print ""
+    #print_product(chargify.Product().getById(161))
+    #print_spacer()
 
-#print_spacer()
+    # Get a Single Product by its Handle
+    #print "Get a Product By its Handle: "
+    #print ""
+    #print_product(chargify.Product().getByHandle('plus'))
+    #print_spacer()
 
+    # Get a Single Product by its Handle
+    print "Save a Subscription: \n"
 
-# Get a Single Product by its ID
-#print "Get a Product By its ID: "
-#print ""
-#print_product(chargify.Product().getById(161))
+    customer = chargify.Customer('customer_attributes')
+    customer.first_name = 'Paul'
+    customer.last_name = 'Trippett'
+    customer.email = 'paul@getyouridx.com'
 
+    creditcard = chargify.CreditCard('credit_card_attributes')
+    creditcard.full_number = 1
+    creditcard.expiration_month = 10
+    creditcard.expiration_year = 2020
 
-#print_spacer()
+    subscription = chargify.Subscription()
+    subscription.product_handle = 'fhaar-mini'
+    subscription.customer = customer
+    subscription.credit_card = creditcard
 
+    if subscription.save():
+        print "Subscription Created Successfully"
+    else:
+        print "Subscription Creation Failed"
 
-# Get a Single Product by its Handle
-#print "Get a Product By its Handle: "
-#print ""
-#print_product(chargify.Product().getByHandle('plus'))
-
-
-#print_spacer()
-
-
-# Get a Single Product by its Handle
-print "Save a Subscription: "
-print ""
-
-customer = chargify.Customer('customer_attributes')
-customer.first_name = 'Paul'
-customer.last_name = 'Trippett'
-customer.email = 'paul@getyouridx.com'
-
-creditcard = chargify.CreditCard('credit_card_attributes')
-creditcard.full_number = 1
-creditcard.expiration_month = 10
-creditcard.expiration_year = 2020
-
-subscription = chargify.Subscription()
-subscription.product_handle = 'fhaar-mini'
-subscription.customer = customer
-subscription.credit_card = creditcard
-
-if subscription.save():
-    print "Subscription Created Successfully"
-else:
-    print "Subscription Creation Failed"
-    
-
+if __name__ == "__main__":
+    run_test()
